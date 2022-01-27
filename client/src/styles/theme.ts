febb324d3b9b42af213c5b8d6f4f503d0b2db272
Marcos Felipe <felipe.oliveira.ext@espm.br>
2022-01-27 11:17:50 -0300

    ajustes2 prettierrc

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -1,10 +1,10 @@
 export default {
   grid: {
     container: '130rem',
-    gutter: '3.2rem',
+    gutter: '3.2rem'
   },
   border: {
-    radius: '0.4rem',
+    radius: '0.4rem'
   },
   font: {
     family:
@@ -19,17 +19,20 @@ export default {
       large: '1.8rem',
       xlarge: '2.0rem',
       xxlarge: '2.8rem',
-    },
+      huge: '5.2rem'
+    }
   },
   colors: {
     primary: '#F231A5',
     secondary: '#3CD3C1',
     mainBg: '#06092B',
+    lightBg: '#F2F2F2',
     white: '#FAFAFA',
     black: '#030517',
     lightGray: '#EAEAEA',
     gray: '#8F8F8F',
     darkGray: '#2E2F42',
+    red: '#FF6347'
   },
   spacings: {
     xxsmall: '0.8rem',
@@ -38,13 +41,17 @@ export default {
     medium: '3.2rem',
     large: '4.0rem',
     xlarge: '4.8rem',
-    xxlarge: '5.6rem',
+    xxlarge: '5.6rem'
   },
   layers: {
     base: 10,
     menu: 20,
     overlay: 30,
     modal: 40,
-    alwaysOnTop: 50,
+    alwaysOnTop: 50
   },
-}
+  transition: {
+    default: '0.3s ease-in-out',
+    fast: '0.1s ease-in-out'
+  }
+} as const
